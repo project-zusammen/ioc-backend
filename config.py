3f29a446512af8a7055ea409d1925c02890f2078
Eko Setiawan <ekomasterwan993@gmail.com>
2024-01-16 15:56:17 +0700

    finish writing the score endpoints tests

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     HOST = str(os.environ.get("DB_HOST", "localhost"))
     DATABASE = str(os.environ.get("DB_DATABASE", "zusammenioctest"))
     USERNAME = str(os.environ.get("DB_USERNAME", "root"))
-    PASSWORD = str(os.environ.get("DB_PASSWORD", "tatapjang"))
+    PASSWORD = str(os.environ.get("DB_PASSWORD", ""))
 
     JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET"))
 
